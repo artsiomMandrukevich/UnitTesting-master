080ea3b400d1a46207885730e36b2bc1651ac6a3
ArtemMandrukevich <fGUy7Zihrn2Q>
2022-08-05 12:17:22 +0200

    Task30

---
+++ src/test/java/webdriver/LocatorsExample.java
@@ -0,0 +1,59 @@
+package webdriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LocatorsExample {
+
+    private WebDriver driver;
+
+    private final By byId = By.id("passp-field-login");
+    private final By byName = By.name("login");
+    private final By byClassName = By.className("AuthSocialBlock-title");
+    private final By byTagName = By.tagName("button");
+    private final By byLinkText = By.linkText("Help and support");
+    private final By byPartialLinkTest = By.partialLinkText("Help");
+    private final By byCSS = By.cssSelector("button[id='passp:sign-in']");
+    private final By byXPath = By.xpath("//button[@id='passp:sign-in']");
+
+    private void checkElementIsVisibleByLocator(By locator) {
+        WebElement elementByID = driver.findElement(locator);
+        assertTrue(elementByID.isDisplayed());
+    }
+
+    @BeforeEach
+    void startDriver() {
+        WebDriverManager.getInstance(ChromeDriver.class).setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    void locatorsExample() {
+        driver.get("https://passport.yandex.com/auth");
+        checkElementIsVisibleByLocator(byId);
+        checkElementIsVisibleByLocator(byName);
+        checkElementIsVisibleByLocator(byClassName);
+        checkElementIsVisibleByLocator(byTagName);
+        checkElementIsVisibleByLocator(byLinkText);
+        checkElementIsVisibleByLocator(byPartialLinkTest);
+        checkElementIsVisibleByLocator(byCSS);
+        checkElementIsVisibleByLocator(byXPath);
+    }
+
+    @AfterEach
+    void stopDriver() {
+        driver.quit();
+    }
+}
